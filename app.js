890554ae541ce7e24c52c1df0e9c31059ccb0bf1
Karlito7285 <73641416+Karlito7285@users.noreply.github.com>
2021-02-01 10:54:06 +0100

    debut de navbar

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fileupload = require('express-fileupload');
 const expressSession = require('express-session');
 const MongoStore = require('connect-mongo');
 const connectFlash = require('connect-flash');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -15,10 +16,20 @@ mongoose.connect("mongodb://localhost:27017/SerieStream", {useNewUrlParser: true
   });
 
   //Configurer express
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // Handlebars
 app.engine('hbs', exphbs({ extname: 'hbs'}));
 app.set('view engine', 'hbs')
+
+//renvoie la page index
+app.get('/',(req, res)=>{
+    res.render('index')
+});
+
+//Ecoute le serveur
+app.listen(7000,function(){
+    console.log('Le serveur tourne sur le port 7000')
+});
